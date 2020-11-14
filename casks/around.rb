ebb8ae9b4e5a5311f531626bfbf6cc2f2c73fbf3
Ereshkigall <Ishi.thelema@gmail.com>
2020-11-14 21:04:49 +0100

    casks for calibre 4.23.0 and around added

---
+++ casks/around.rb
@@ -0,0 +1,11 @@
+cask "around" do
+  version "0.26.0"
+  sha256 "b43d47a55d04efeea4ae020c72f5265774aa2c7f575d4d25340b2c4a6354e4ac"
+
+  url "https://downloads.around.co/Around-#{version}.dmg"
+  name "Around"
+  desc "video call/meeting app"
+  homepage "https://www.around.co/"
+
+  app "Around.app"
+end
