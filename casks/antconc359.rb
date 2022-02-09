b292bdf566b39db985168d25e148ba6b6ab3b2dd
Ereshkigall <Ishi.thelema@gmail.com>
2022-02-09 21:23:17 +0100

    finished antconc commit

--- casks/antconc359.rb
+++ casks/antconc359.rb
@@ -1,11 +1,18 @@
-cask "antconc-at-359" do
+cask "antconc359" do
   version "3.5.9"
   sha256 "faac413a4bd914feec6609b96fcf368fa6e91baeedbd15303a8a0594a38db7dd"
 
   url "https://www.laurenceanthony.net/software/antconc/releases/AntConc359/AntConc.zip"
-  name "antconc@3.5.9"
+  name "AntConc"
   desc "A freeware corpus analysis toolkit for concordancing and text analysis. Stable version 3.5.9"
   homepage "https://www.laurenceanthony.net/software/antconc/"
 
-  app "Antconc3.5.9"
+  livecheck do
+    url :homepage
+    regex(/>\s*Mac[^<]*\((\d+(?:\.\d+)+)\)\s*</i)
+  end
+
+  app "AntConc.app"
+
+  zap trash: "~/Library/Preferences/AntConc.plist"
 end
