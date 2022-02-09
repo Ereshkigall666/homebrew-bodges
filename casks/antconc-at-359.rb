1e9db1950f3737cc6a0829acdb8bf98ed6cbf6bb
Ereshkigall <Ishi.thelema@gmail.com>
2022-02-09 15:13:35 +0100

    added antconc

---
+++ casks/antconc-at-359.rb
@@ -0,0 +1,11 @@
+cask "antconc-at-359" do
+  version "3.5.9"
+  sha256 "faac413a4bd914feec6609b96fcf368fa6e91baeedbd15303a8a0594a38db7dd"
+
+  url "https://www.laurenceanthony.net/software/antconc/releases/AntConc359/AntConc.zip"
+  name "antconc@3.5.9"
+  desc "A freeware corpus analysis toolkit for concordancing and text analysis. Stable version 3.5.9"
+  homepage "https://www.laurenceanthony.net/software/antconc/"
+
+  app "Antconc3.5.9"
+end
